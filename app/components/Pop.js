c63b1076ee6fcb78f87dd9d388d9b84692d1bb3a
Sovidi <lim132445@gmail.com>
2024-02-19 23:08:32 +0900

    pop2

--- app/components/Pop.js
+++ app/components/Pop.js
@@ -3,13 +3,26 @@ import styles from "../page.module.scss"
 import { myContext } from './Context'
 
 function Pop() {
-    const {popSt, setPopSt, pData, setPData} = useContext(myContext);
+	const { popSt, setPopSt, pData, setPData } = useContext(myContext);
 
-  return (
-    <div onClick={()=>{setPopSt("")}} className={`${styles.popBg} ${popSt == "active" ? styles.active : ""}`}>
-        <div className={styles.popBox}>{pData.title}</div>
-    </div>
-  )
+	return (
+		<div onClick={() => { setPopSt("") }} className={`${styles.popBg} ${popSt == "active" ? styles.active : ""}`}>
+			<div className={styles.popBox}>
+				<div className={styles.bInfo}>
+					<figure className={styles.bPoster}><img src={pData.image} /></figure>
+					<div className={styles.infoText}>
+						<p className={styles.title}>{pData.title}</p>
+						<p className={styles.author}>{pData.author}</p>
+						<p className={styles.price}>규정가 {pData.discount} 원</p>
+						<p className={styles.pub}>{pData.publisher}</p>
+					</div>
+				</div>
+				<div className={styles.descBox}>
+					<p className={styles.bDesc}>{pData.description}</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Pop
